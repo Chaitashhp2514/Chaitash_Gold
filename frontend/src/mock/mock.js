778d0c238892d2676eea84a0790ebe7d64a72281
Chaitashhp2514 <chaitash.work@gmail.com>
2026-04-25 03:56:01 +0530

    Update mock.js

--- frontend/src/mock/mock.js
+++ frontend/src/mock/mock.js
@@ -14,7 +14,7 @@ export const profile = {
   phone: "+91 70692 91955",
   address: "34, Rajhans Society, Opp. Rutu Ice-cream, Nr. APC, Anand",
   photo:
-    "https://i.ibb.co/zWGxzphG/IMG-5132.png",
+    "https://i.ibb.co/QFZt1Pjv/8-A7-AE6606.png",
   resumeUrl:
     "https://drive.google.com/file/d/1qa3xFDgxEK9xmhXwblXN-f0U_Q8CUwQF/view?usp=sharing",
   social: {
